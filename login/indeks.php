e8cdefaf92731a67667e051f18e2a896aef66422
mutma innah <mutmainnah1719@gmail.com>
2024-06-28 14:05:17 +0700

    Add files via upload

---
+++ login/indeks.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+
+</head>
+<body>
+    <div id="form">
+    <form action="" method="post">
+        <h4>selamat datang</h4>
+    <p>silahkan masukkan email dan password</p>
+    <div class="form-logon">
+    <label for="">email</label>
+    <input type="text"name="username" placeholder="masukkan email">
+    </div>
+
+    <div class="form.login">
+    <label for=""></label>
+    <label for="">pasword</label>
+    <input type="pasword"name="pasword" placeholder="masukan pasword">
+    </div>
+
+     <div>
+    <a href="forgot.php"> lupa pasword</a>
+    <div class="form.login"></label>
+    <button>login sekarang </button>
+    </div>
+
+    <center><a href="register.php"> belum punya akun? daftar sekarang</a><center>
+
+
+
+
+
+</form>
+</div>
+
+</body>
+</html>
---
+++ login/reset.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+
+</head>
+<body>
+    <div id="form">
+    <form action="proses.php" method="post">
+        <h4>selamat datang</h4>
+    <p>silahkan masukkan email dan password</p>
+    <div class="form-login">
+    <label for="">email</label>
+    <input type="text"name="username" placeholder="masukkan email">
+    </div>
+
+     <div>
+    <a href="forgot.php"> lupa pasword</a>
+    <div class="form.login"></label>
+    <button> reset </button>
+    </div>
+
+    <center><a href="register.php"> belum punya akun? daftar sekarang</a><center>
+
+
+
+
+
+</form>
+</div>
+
+</body>
+</html>
